65f2cb29116df1f326f0619673d1f5612d95fcfc
NXH <neilhartz@Neils-MacBook-Air.local>
2013-10-22 15:53:35 -0500

    Setting up the app

---
+++ bulletin/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bulletin::Application.config.secret_key_base = '9f80e8046e635c603036afcc43799d2cd000c6b790f80b9798efe58b6109c984ea5e7d4fbe37defffe9b2bd7ac95bd730081d1d10012f889624079d66754d51d'
---
+++ bulletin/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bulletin::Application.config.session_store :cookie_store, key: '_bulletin_session'
