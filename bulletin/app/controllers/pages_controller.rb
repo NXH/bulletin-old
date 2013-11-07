28e461f5e3601b64fb1a70e0fd4365814c408896
NXH <neil.hartz@gmail.com>
2013-11-07 12:22:58 -0600

    Adding create functionality to Accounts and Events
    Accounts working, bug issue in Events still needs to be resolved on
    form submission

--- bulletin/app/controllers/pages_controller.rb
+++ bulletin/app/controllers/pages_controller.rb
@@ -1,12 +1,20 @@
 class PagesController < ApplicationController
   def landing
-    render('landing')
+
+  end
+  def new_account
+
   end
   def create_account
-    render('create_account')
+    @t = Calendar.new
+    @t.person1 = params[:person1]
+    @t.person2 = params[:person2]
+    @t.nickname = params[:nickname]
+    @t.password_digest = params[:password_digest]
+    @t.save
   end
   def user_home
-    render('user_home')
+
   end
   def event
     @event_num = params[:event_id].to_i
@@ -18,5 +26,16 @@ class PagesController < ApplicationController
     @event_cat = Event.find(@event_num).category_id
     @event_p1 = Event.find(@event_num).person1
     @event_p2 = Event.find(@event_num).person2
+  end
+  def create_event
+    @e = Event.new
+    @e.title = params[:title]
+    @e.description = params[:description]
+    @e.location = params[:location]
+    @e.start_time = params[:start_time]
+    @e.end_time = params[:end_time]
+    @e.person1 = params[:person1]
+    @e.person2 = params[:person2]
+    @e.save
   end
 end
--- bulletin/config/routes.rb
+++ bulletin/config/routes.rb
@@ -1,61 +1,9 @@
 Bulletin::Application.routes.draw do
   get("/landing", {:controller => 'pages', :action => 'landing'})
-  get("/user-home", {:controller => 'pages', :action => 'user_home'})
-  get("/create-account", {:controller => 'pages', :action => 'create_account'})
+  get("/user_home", {:controller => 'pages', :action => 'user_home'})
+  get("/new_account", {:controller => 'pages', :action => 'new_account'})
+  get("/create_account", {:controller => 'pages', :action => 'create_account'})
   get("/event/:event_id", {:controller => 'pages', :action => 'event'})
+  get("/create_event", {:controller => 'pages', :action => 'create_event'})
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
